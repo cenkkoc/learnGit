96af60f01fbbf7acc8c3199df5d91a09162319b2
Cenk KOC <cenk.koc@gmail.com>
2020-04-25 15:08:37 +0300

    demo2

--- netconf-1.py
+++ netconf-1.py
@@ -1,7 +1,7 @@
 from ncclient import manager
 
 
-router = {"host": "ios-xe-mgmt-latest.cisco.com", "port": "10000", "username": "developer", "password": "C1sco12345"}
+router = {"host": "ios-xe-mgmt-latest.cisco.com", "port": "10000", "username": "developer", "password": "C1sco123455"}
 
 with manager.connect(host=router["host"],port=router["port"], username=router["username"], password=router["password"], hostkey_verify=False) as m:
     m.close_session()
